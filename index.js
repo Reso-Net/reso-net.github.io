a086ceb3c98ecf9d4776f7e64a783184d8767abc
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-08-26 22:20:13 +1000

    Basic landing page

--- index.js
+++ index.js
@@ -4,3 +4,11 @@
 // Yellow Green #9BC53D
 // Persian Red #C3423F
 // Eerie Black #1B1E1E
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const header = document.getElementById("header");
+    header.className = "visibleA";    
+    
+    const creator = document.getElementById("creator");
+    creator.className = "visibleB";
+});
